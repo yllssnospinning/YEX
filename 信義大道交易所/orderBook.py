27f850fcdcd2bcf5f352aa5b2d6d33ff8a01f2dc
yllssnospinning <mrnospinning@gmail.com>
2025-03-18 22:00:38 +0800

    Update orderBook.py

--- 信義大道交易所/orderBook.py
+++ 信義大道交易所/orderBook.py
@@ -1,4 +1,5 @@
 from orderLevel import orderLevel as level
+from order import Order as order
 
 class orderBook:
     def __init__(self, instrumentID, tickSize):
@@ -11,11 +12,27 @@ class orderBook:
         rightInstrument = order.instrumentID == self.instrumentID
         if rightInstrument:
             isMarketOrder = order.type == 'mkt'
+            order.price = round(order.price / self.tickSize) * self.tickSize
             if isMarketOrder:
+                #print('posting market order')
                 self.mktOrders[order.side].postOrder(order)
             else:
                 priceLevel = round(order.price / self.tickSize) * self.tickSize
-                limitLevel = self.lob[order.type]
+                limitLevel = self.lob[order.side]
                 if not priceLevel in limitLevel:
-                    limitLevel[priceLevel] = level(self.instrumentID, order.type, priceLevel)
-                
+                    limitLevel[priceLevel] = level(self.instrumentID, order.side, priceLevel)
+                limitLevel[priceLevel].postOrder(order)
+
+
+orders = orderBook('YLLSS', 5)
+orders.postOrder(order('YLLSS', 1, 'Hairo', 'mkt', 100, 10, 1))
+orders.postOrder(order('YLLSS', 2, 'Hairo', 'lim', 92, 10, 1))
+
+#book = orders.mktOrders[1]
+#print(book.side)
+#print(book.type)
+
+book = orders.lob[1]
+
+
+                 
