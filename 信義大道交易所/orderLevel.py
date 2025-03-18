c4917a1318bd1f24f61a2e4d8ee044be6dcbdf76
yllssnospinning <mrnospinning@gmail.com>
2025-03-18 22:01:06 +0800

    Update orderLevel.py

--- 信義大道交易所/orderLevel.py
+++ 信義大道交易所/orderLevel.py
@@ -1,24 +1,34 @@
 
 class orderLevel:
     def __init__(self, instrumentID, side, price):
-        self.price = float(price)
-        self.instrument = str(instrument)
+        try:
+            self.price = float(price)
+        except:
+            self.price = price
+        self.instrument = str(instrumentID)
         self.side = int(side)
         self.book = {}
+        self.type = 'lim' if price != 'mkt' else 'mkt'
 
     def postOrder(self, order):
         orderPriority = order.prio
-        rightInstrument = order.instrument == self.instrument
-        rightPrice = order.price = self.price
+        rightInstrument = order.instrumentID == self.instrument
+        rightPrice = order.price == self.price
+        if self.type == 'mkt':
+            rightPrice = True
         if rightInstrument and rightPrice:
             if orderPriority in self.book:
                 self.book[orderPriority].append(order)
             else:
+                #print('creating new prio level')
                 self.book[orderPriority] = [order]
+                # print(self.book)
                 self.sortBook()
+        else:
+            print('cannot post order!')
     
     def sortBook(self):
-        self.book = [self.book[i] for i in sorted(self.book.keys)]
+        self.book = {i:self.book[i] for i in sorted(self.book.keys())}
     
     @property
     def levelRecentTimeQty(self):
