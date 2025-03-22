857c62a6dfa492c7079dbb2f285b4b30e14a60a7
yllssnospinning <mrnospinning@gmail.com>
2025-03-22 10:21:56 +0800

    Update orderLevel.py

--- 信義大道交易所/orderLevel.py
+++ 信義大道交易所/orderLevel.py
@@ -1,60 +1,86 @@
+from orderLevel import orderLevel as level
+from order import Order as order
 
-class orderLevel:
-    def __init__(self, instrumentID, side, price):
-        try:
-            self.price = float(price)
-        except:
-            self.price = price
-        self.instrument = str(instrumentID)
-        self.side = int(side)
-        self.book = {}
-        self.type = 'lim' if price != 'mkt' else 'mkt'
-
-    def postOrder(self, order):
-        orderPriority = order.prio
-        rightInstrument = order.instrumentID == self.instrument
-        rightPrice = order.price == self.price
-        if self.type == 'mkt':
-            rightPrice = True
-        if rightInstrument and rightPrice:
-            if orderPriority in self.book:
-                self.book[orderPriority].append(order)
-            else:
-                #print('creating new prio level')
-                self.book[orderPriority] = [order]
-                # print(self.book)
-                self.sortBook()
-        else:
-            print('cannot post order!')
+class orderBook:
+    def __init__(self, instrumentID, tickSize):
+        self.tickSize = float(tickSize)
+        self.instrumentID = str(instrumentID)
+        self.lob = {1:{}, 0:{}}
+        self.mktOrders = {1:{}, 0:{}}
     
-    def sortBook(self):
-        self.book = {i:self.book[i] for i in sorted(self.book.keys())}
+    def postOrder(self, order):
+        rightInstrument = order.instrumentID == self.instrumentID
+        if rightInstrument:
+            isMarketOrder = order.type == 'mkt'
+            order.price = round(order.price / self.tickSize) * self.tickSize
+            if isMarketOrder:
+                priceLevel = round(order.price / self.tickSize) * self.tickSize
+                limitLevel = self.mktOrders[order.side]
+                if not priceLevel in limitLevel:
+                    limitLevel[priceLevel] = level(self.instrumentID, order.side, priceLevel)
+                limitLevel[priceLevel].postOrder(order)
+            else:
+                priceLevel = round(order.price / self.tickSize) * self.tickSize
+                limitLevel = self.lob[order.side]
+                if not priceLevel in limitLevel:
+                    limitLevel[priceLevel] = level(self.instrumentID, order.side, priceLevel)
+                limitLevel[priceLevel].postOrder(order)
+
+    @property
+    def bestBidAsk(self):
+        bidLevels = list(self.lob[1].keys())
+        askLevels = list(self.lob[0].keys())
+        bestBid = 'null' if len(bidLevels) == 0 else bidLevels[0]
+        bestAsk = 'null' if len(askLevels) == 0 else askLevels[0]
+        bbTimeQty, baTimeQty = 'null', 'null'
+        if not bestBid == 'null':
+            bbTimeQty = self.lob[1][bestBid].levelRecentTimeQty            
+        if not bestAsk == 'null':
+            baTimeQty = self.lob[0][bestAsk].levelRecentTimeQty
+        return bestBid, bestAsk, bbTimeQty, baTimeQty
     
     @property
-    def levelRecentTimeQty(self):
-        recentTime, totalQty = -1, 0
-        for i in self.book:
-            orders = self.book[i]
-            for ii in orders:
-                order = orders[ii]
-                if recentTime == -1:
-                    recentTime = order.orderID
-                totalQty += abs(order.qty)
-        
-        return recentTime, totalQty
+    def bestMarketLimits(self):
+        bidLevels = list(self.mktOrders[1].keys())
+        askLevels = list(self.mktOrders[0].keys())
+        bestBid = 'null' if len(bidLevels) == 0 else bidLevels[0]
+        bestAsk = 'null' if len(askLevels) == 0 else askLevels[0]
+        bbTimeQty, baTimeQty = 'null', 'null'
+        if not bestBid == 'null':
+            bbTimeQty = self.mktOrders[1][bestBid].levelRecentTimeQty            
+        if not bestAsk == 'null':
+            baTimeQty = self.mktOrders[0][bestAsk].levelRecentTimeQty
+        return bestBid, bestAsk, bbTimeQty, baTimeQty
+    # TODO
     
-    def fillOrders(self, qty):
-        amountToFill = float(qty)
-        filledOrders = []
-        for i in self.book:
-            orders = self.book[i]
-            for ii in orders:
-                if amountToFill == 0:
-                    return []
-                order = orders[ii]
-                orderFillQty = min(order.qty, amountToFill)
-                order.qty -= orderFillQty
-                amountToFill -= orderFillQty
-                filledOrders.append([order.orderID, orderFillQty])
-        return filledOrders
+    @property
+    def aggressingMarketOrder(self):
+        buySideAggressing, sellSideAggressing = False, False
+        mktLimits = self.bestMarketLimits
+        bbo = self.bestBidAsk
         
+        # check market buy side for fillability
+        if mktLimits[0] > bbo[1]:
+            buySideAggressing = True
+        if mktLimits[1] < bbo[0]:
+            sellSideAggressing = True
+        # TODO
+        
+
+    def fillOrder(self):
+        pass
+
+# TESTING:IGNORE
+orders = orderBook('YLLSS', 5)
+orders.postOrder(order('YLLSS', 1, 'Hairo', 'mkt', 100, -10, 1))
+orders.postOrder(order('YLLSS', 2, 'Lychee', 'mkt', 100, -30, 2))
+orders.postOrder(order('YLLSS', 3, 'Hairo', 'lim', 92, 100, 1))
+orders.postOrder(order('YLLSS', 4, 'YCL', 'lim', 80, 10, 2))
+
+#book = orders.mktOrders[1]
+#print(book.side)
+#print(book.type)
+print(orders.bestBidAsk)
+print(orders.bestMarketLimits)
+book = orders.mktOrders[1]
+#print(book)
