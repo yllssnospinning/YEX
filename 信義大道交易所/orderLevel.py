cc2389ee4d820df3133b32eeb52eb4a65efbfa85
yllssnospinning <mrnospinning@gmail.com>
2025-03-24 22:22:39 +0800

    Minor changes to levelRecentTimeQty

--- 信義大道交易所/orderLevel.py
+++ 信義大道交易所/orderLevel.py
@@ -1,21 +1,13 @@
+from order import Order as order
 class orderLevel:
-    def __init__(self, instrumentID, side, price):
-        try:
-            self.price = float(price)
-        except:
-            self.price = price
+    def __init__(self, instrumentID):
         self.instrument = str(instrumentID)
-        self.side = int(side)
         self.book = {}
-        self.type = 'lim' if price != 'mkt' else 'mkt'
 
     def postOrder(self, order):
         orderPriority = order.prio
         rightInstrument = order.instrumentID == self.instrument
-        rightPrice = order.price == self.price
-        if self.type == 'mkt':
-            rightPrice = True
-        if rightInstrument and rightPrice:
+        if rightInstrument:
             if orderPriority in self.book:
                 self.book[orderPriority].append(order)
             else:
@@ -29,8 +21,8 @@ class orderLevel:
     def sortBook(self):
         self.book = {i:self.book[i] for i in sorted(self.book.keys())}
     
-    @property
-    def levelRecentTimeQty(self):
+    def levelRecentTimeQty(self, timeOnly):
+        # timeOnly = True
         recentTime, totalQty = -1, 0
         for i in self.book:
             orders = self.book[i]
@@ -38,28 +30,29 @@ class orderLevel:
                 order = ii
                 if recentTime == -1:
                     recentTime = order.orderID
+                    if timeOnly:
+                        return recentTime
                 totalQty += abs(order.qty)
-        
         return recentTime, totalQty
     
-    def fillOrders(self, qty):
-        amountToFill = float(qty)
+    def fillOrders(self, qty, incomingOrderName):
+        amountToFill = qty
         filledOrders = []
-        for i in self.book:
-            orders = self.book[i]
-            for i, ii in enumerate(orders):
-                if amountToFill == 0:
-                    return filledOrders
-                order = ii
-                print(order.qty, amountToFill)
-                orderFillQty = min(abs(order.qty), amountToFill)
-                if order.qty > 0:
+        totalFilledQty = 0
+        for prio in self.book:
+            orders = self.book[prio]
+            removedOrder = []
+            for index, order in enumerate(orders):
+                orderFillQty = min(order.qty, amountToFill)
+                if order.traderID != incomingOrderName:
                     order.qty -= orderFillQty
-                else:
-                    order.qty += orderFillQty
-                print(orderFillQty)
-                if order.qty == 0:
-                    orders.pop(i)
-                amountToFill -= orderFillQty
-                filledOrders.append([order.orderID, orderFillQty])
-        return filledOrders
+                    totalFilledQty += orderFillQty
+                    amountToFill -= orderFillQty
+                    if orderFillQty != 0:
+                        filledOrders.append([order.orderID, orderFillQty])
+                        removedOrder.append(index)
+            print(removedOrder)
+            print('orders', orders)
+            for i, ii in enumerate(removedOrder):
+                orders.pop(ii - i)
+        return filledOrders, totalFilledQty
